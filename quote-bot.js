4749a5b683e2e065274c62cdd884b1cdce75a467
la Fabryk Web Crafts <l.nassimo@gmail.com>
2019-06-25 15:06:50 +0200

    mon premier commit

--- quote-bot.js
+++ quote-bot.js
@@ -1,17 +1,17 @@
 var Twit = require('twit');
 
 var T = new Twit({
-	consumer_key: '...'
-	, consumer_secret: '...'
-	, access_token: '...'
-	, access_token_secret: '...'
+	consumer_key: 'qfKt8TQisJv4kEbWrvqiLZuRR'
+	, consumer_secret: '8On1e6fW3df4o1ZJPfaX34szjuXMFRiRfFpsGrL6v1iDDtln7t'
+	, access_token: '1133723453251608579-VMsOaPmuaxBdW80JnneVyonOCaumaS'
+	, access_token_secret: 'etVB4L7fmfWg5ABz0dFjiz4rxRy6vsJk7y370D94P2VWM'
 });
 
 function retweetRecent() {
-	T.get('search/tweets', {q: "#coldfusion OR #cfml", result_type: "recent"}, function (err, data,response) {
+	T.get('search/tweets', {q: "#france OR #auto-entrepreneur OR #entrepreneur", result_type: "recent"}, function (err, data,response) {
 		if (!err) {
 			var tweet = data.statuses[0];
-			var retweetBody = 'RT @' + tweet.user.screen_name + ' ' + tweet.text;
+			var retweetBody = 'Gagnez des euros avec amazon https://amzn.to/2Yb4JBN RT @' + tweet.text;
 			T.post('statuses/update',{status:retweetBody}, function (err,response) {
 				if (response) {
 					console.log('Quote Tweeted Tweet ID: ' + tweet.id_str);
